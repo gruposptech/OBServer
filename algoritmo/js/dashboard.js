b8a65b2516d5affc1ddda5c324e44e6d014003e9
brenokas <brenofreitas28f@gmail.com>
2025-05-20 19:02:15 -0300

    Adicionando o nome da sala selecionada ao título da página e alterando identação do código

--- algoritmo/js/dashboard.js
+++ algoritmo/js/dashboard.js
@@ -1,61 +1,73 @@
 function exibeDataHora() {
-  // selecionadno a div que exibe a data e a hora na dashboard pelo ID
-  const divDataHora = document.getElementById("data-hora");
+	// selecionadno a div que exibe a data e a hora na dashboard pelo ID
+	const divDataHora = document.getElementById('data-hora');
 
-  // atualizando a hora a cada 1 minuto
-  setInterval(() => {
-    // pegando a data atual e colocando em uma variável
-    const data = new Date();
+	// atualizando a hora a cada 1 minuto
+	setInterval(() => {
+		// pegando a data atual e colocando em uma variável
+		const data = new Date();
 
-    // inserindo a data e hora formatadas para o nosso local (pt-BR) na div que selecionei
-    divDataHora.innerHTML = `${data.toLocaleDateString("pt-BR"
-)} - ${data.toLocaleTimeString("pt-BR")}`;
-  }, 1000);
+		// inserindo a data e hora formatadas para o nosso local (pt-BR) na div que selecionei
+		divDataHora.innerHTML = `${data.toLocaleDateString('pt-BR')} - ${data.toLocaleTimeString('pt-BR')}`;
+	}, 1000);
 }
 
 function trocaTitulo(checkBox) {
-  //selecionando o span que exibe o nome da sala no topo da tela
-  var spanTitulo = document.getElementById("nome-sala-titulo");
+	//selecionando o span que exibe o nome da sala no topo da tela
+	var spanTitulo = document.getElementById('nome-sala-titulo');
+  // selecionando todas as checkbox
+	var checkBoxes = document.getElementsByClassName('checkbox-sala');
+  // criando uma variável para guardar o titulo da sala
+	var titulo = '';
 
-  // se a checkBox passada como parâmetro está marcada...
-  if (checkBox.checked) {
+	// laço for que percorre todas as checkboxes
+	for (var i = 0; i < checkBoxes.length; i++) {
+		// a variável checkBox é a checkbox da posição i da lista checkBoxes
+		var checkBox = checkBoxes[i];
 
-    // guardando todas as checkboxes na variável checkBoxes
-    var checkBoxes = document.getElementsByClassName("checkbox-sala");
+		//se a checkBox está marcada...
+		if (checkBox.checked) {
+			var nomeSala = '';
 
-    // percorrendo todas as checkboxes
-    for (var i = 0; i < checkBoxes.length; i++) {
+			// checando qual checkbox está marcada e atriuindo o nome dela a variavel nomeSala
+			if (checkBox.name == 'sala-1') {
+				nomeSala = 'Datacenter principal';
+			} else if (checkBox.name == 'sala-2') {
+				nomeSala = 'Sala de backup';
+			} else if (checkBox.name == 'sala-3') {
+				nomeSala = 'Sala de servidores A';
+			} else if (checkBox.name == 'sala-4') {
+				nomeSala = 'Sala de servidores B';
+			} else if (checkBox.name == 'sala-5') {
+				nomeSala = 'Sala de servidores C';
+			}
 
-      // se a checkbox que está sendo verificada for diferente da checkbox passada como parâmentro da função (que é a checkbox marcada)
-      // então eu desmarco ela usando o .checked = false
-      if (checkBoxes[i] != checkBox) {
-        checkBoxes[i].checked = false;
-      }
-    }
+			// se o título não está vazio, eu adiciono uma vírgula para separar os nomes
+			if (titulo != '') {
+				titulo += ', ';
+			}
 
-    // aqui eu checo qual o nome da checkbox que foi passada como parâmetro e troco o título da página de acordo.
-    if (checkBox.name == "sala-1") {
-      spanTitulo.innerHTML = "Datacenter principal";
-    } else if (checkBox.name == "sala-2") {
-      spanTitulo.innerHTML = "Sala de backup";
-    } else {
-      spanTitulo.innerHTML = "Sala de servidores A";
-    }
-  }
+			// adicionando o nome da sala a variavel título
+			titulo += nomeSala;
+		}
+	}
+
+	// trocando o span do título para a variável título
+	spanTitulo.innerHTML = titulo;
 }
 
 // window.onload: método para executar uma função assim que a janela for carregada
 window.onload = () => {
-  // então, estou executando a função de exibir a data e hora assim que a tela carrega
-  exibeDataHora();
+	// então, estou executando a função de exibir a data e hora assim que a tela carrega
+	exibeDataHora();
 
-  // aqui, estou procurando através de um laço for, qual checkbox está marcada
-  // quando acho, executo a função trocaTitulo() passando a dashboard marcada como parâmetro
-  var checkBoxes = document.getElementsByClassName("checkbox-sala");
-  for (var i = 0; i < checkBoxes.length; i++) {
-    if (checkBoxes[i].checked) {
-      trocaTitulo(checkBoxes[i]);
-      break;
-    }
-  }
+	// aqui, estou procurando através de um laço for, qual checkbox está marcada
+	// quando acho, executo a função trocaTitulo() passando a dashboard marcada como parâmetro
+	var checkBoxes = document.getElementsByClassName('checkbox-sala');
+	for (var i = 0; i < checkBoxes.length; i++) {
+		if (checkBoxes[i].checked) {
+			trocaTitulo(checkBoxes[i]);
+			break;
+		}
+	}
 };
