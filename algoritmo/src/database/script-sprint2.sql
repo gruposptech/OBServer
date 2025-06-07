810fe53bc34cc601004b31b646b007993e1f31c2
brenokas <brenofreitas28f@gmail.com>
2025-06-07 11:07:03 -0300

    Atualizando o script e a modelagem do banco de dados

--- algoritmo/src/database/script-sprint2.sql
+++ algoritmo/src/database/script-sprint2.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS empresa (
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     nomeEmpresa VARCHAR(100) NOT NULL,             
     cnpjEmpresa CHAR(14) NOT NULL UNIQUE,            
-    emailEmpresa VARCHAR(100) NOT NULL,                                   
+    telefoneEmpresa CHAR(11) NOT NULL,                                   
     codigoCadastro CHAR(5) NOT NULL UNIQUE
 );
 
