d907c89e580b01ff476c6e252e3d5c437d8549cf
brenokas <brenofreitas28f@gmail.com>
2025-06-08 11:24:03 -0300

    Cadastro de empresas funcionando

--- algoritmo/src/controllers/usuarioController.js
+++ algoritmo/src/controllers/usuarioController.js
@@ -56,18 +56,12 @@ function cadastrar(req, res) {
 	// Crie uma variável que vá recuperar os valores do arquivo cadastro.html
 	var nome = req.body.nomeServer;
 	var cnpj = req.body.cnpjServer;
-	var email = req.body.emailServer;
 	var telefone = req.body.telefoneServer;
-	var senha = req.body.senhaServer;
 	var codigo = req.body.codigoServer;
 
 	// Faça as validações dos valores
 	if (nome == undefined) {
 		res.status(400).send('Seu nome está undefined!');
-	} else if (email == undefined) {
-		res.status(400).send('Seu email está undefined!');
-	} else if (senha == undefined) {
-		res.status(400).send('Sua senha está undefined!');
 	} else if (cnpj == undefined) {
 		res.status(400).send('Seu cnpj está undefined!');
 	} else if (telefone == undefined) {
@@ -77,7 +71,7 @@ function cadastrar(req, res) {
 	} else {
 		// Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
 		usuarioModel
-			.cadastrar(nome, cnpj, email, telefone, senha, codigo)
+			.cadastrar(nome, cnpj, telefone, codigo)
 			.then(function (resultado) {
 				res.json(resultado);
 			})
@@ -179,5 +173,5 @@ module.exports = {
 	temUsuarioCadastrado,
 	cadastrarParametros,
 	pegarParametros,
-	atualizarParametrosSensores
+	atualizarParametrosSensores,
 };
--- algoritmo/src/models/usuarioModel.js
+++ algoritmo/src/models/usuarioModel.js
@@ -17,21 +17,19 @@ function autenticar(cnpj, senha, email) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, cnpj, email, telefone, senha, codigo) {
+function cadastrar(nome, cnpj, telefone, codigo) {
 	console.log(
 		"ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
 		nome,
 		cnpj,
-		email,
 		telefone,
-		senha,
 		codigo
 	);
 
 	// Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
 	//  e na ordem de inserção dos dados.
 	var instrucaoSql = `
-        INSERT INTO empresa (nomeEmpresa, cnpjEmpresa, emailEmpresa, telefoneEmpresa, senhaEmpresa, codigoCadastro) VALUES ('${nome}', '${cnpj}', '${email}', '${telefone}', '${senha}', '${codigo}');
+        INSERT INTO empresa (nomeEmpresa, cnpjEmpresa, telefoneEmpresa, codigoCadastro) VALUES ('${nome}', '${cnpj}', '${telefone}', '${codigo}');
     `;
 	console.log('Executando a instrução SQL: \n' + instrucaoSql);
 	return database.executar(instrucaoSql);
